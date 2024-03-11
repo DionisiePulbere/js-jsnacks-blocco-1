4752a69fbcf0abfac0e786681d0d5277be69c3f1
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-11 16:21:40 +0100

    feat: add jsnack-5

--- jsnack-5/js/script.js
+++ jsnack-5/js/script.js
@@ -1,1 +1,20 @@
 // - Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+// stabilisco quati numeri voglio
+const howManyNumbers = 10; 
+// creo un'array vuota 
+const numbers = [];
+
+let bigger = 0;
+// chiedo all'utente 10 numeri utilizzando il ciclo for 
+for (let i = 0; i < howManyNumbers; i++){
+    const userNumber = parseInt(prompt("Inserisci un numero"));
+    numbers.push(userNumber);
+    // stabilisco il più grande
+    if (numbers[i] > bigger){
+        bigger = numbers[i];
+    }
+}
+// stampo il risultato
+console.log(numbers);
+console.log(bigger);
