16ce6cfbbaa82c8e8f4c7cabbabffbe8867aa1dd
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-11 15:42:58 +0100

    feat: add jsnack1

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,2 +1,26 @@
 // - Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare 
 // duplicati (inserire il numero nell'array solo se non è già presente).
+
+// creo un'array vuota 
+const howManyNumbers = 5; 
+const numbers = [];
+
+
+// chiedo all'utente 5 numeri utilizzando il ciclo for 
+for (let i = 0; i < howManyNumbers; i++){
+    const userNumber = parseInt(prompt("Inserisci un numero"));
+
+    // controllo se non sono già stati inserirti 
+    if (!numbers.includes(userNumber)){
+        // inserisco all'interno dell'array
+        numbers.push(userNumber);
+    }
+}
+
+console.log(numbers);
+
+
+
+    
+
+    
